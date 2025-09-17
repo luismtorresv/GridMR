9dcf1918a228b5b95b1aa5f3cde779b8d0ab6b0e
Luis Torres <lmtorresv@eafit.edu.co>
2025-09-16 22:09:37 -0500

    Set up a primitive first approach to FastAPI

--- master.py
+++ master.py
@@ -12,3 +12,18 @@ def handle_master(args: argparse.Namespace):
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/job/submit")
+def receive_job(data_url: str, code_url: str):
+    return {"data_url": data_url, "code_url": code_url}
+
+
+@app.get("/job/status/{job_id}")
+def send_status(job_id: int):
+    return {"job_id": job_id}
+
+
+@app.get("/job/result/url/{job_id}")
+def send_result_url(job_id: int):
+    return {"result_url": job_id}
